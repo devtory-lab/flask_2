ec6cd06c96a080d888fd9a387e1aeb0fb52001e0
devtory-lab <an44andedu@gmail.com>
2021-03-31 17:28:06 +0900

    ch03 branch app py : url_for add base page : css add index page : form add main css : add

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
