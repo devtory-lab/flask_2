8babeaa4ebb5f732916cb3435a6953bb1b3521ba
devtory-lab <an44andedu@gmail.com>
2021-03-31 17:12:25 +0900

    ch03 branch change class name : Todo index page methods separate. get, post tasks : order by credte

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class Todl(db.Model):
+class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     credte = db.Column(db.DateTime, default=datetime.utcnow)
@@ -15,9 +15,13 @@ class Todl(db.Model):
         return '<Task %r>' % self.id
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        pass
+    else:
+        tasks = Todo.query.order_by(Todo.credte).all()
+        return render_template('index.html', tasks=tasks)
 
 
 
